80ec8ed622a78b8bda7f2d21167b67ed39992518
Joan Vilarrasa <joan.vilarrasa96@gmail.com>
2026-05-25 21:28:02 +0200

    small change to tabls

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ do
   --   See `:help lua-options`
   --   and `:help lua-guide-options`
   vim.o.list = true
-  vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+  vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
   -- Preview substitutions live, as you type!
   vim.o.inccommand = 'split'
